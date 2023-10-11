f7834ec3ebabcdc968eb289e50096c8f6471c56e
LENOVO <test23062000@gmail.com>
2023-10-11 19:18:20 +0300

    changed main

---
+++ src/home_one/Golf.java
@@ -0,0 +1,13 @@
+package home_one;
+
+public class Golf {
+    private int numberCar;
+
+    public Golf(int numberCar) {
+        this.numberCar = numberCar;
+    }
+
+    public int getNumberCar() {
+        return numberCar;
+    }
+}
